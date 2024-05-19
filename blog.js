def23f294a13b268a86352b5fd64a1fcbde60c43
RadonZ <supachai27102000@gmail.com>
2024-05-19 20:58:47 +0700

    search fix

--- blog.js
+++ blog.js
@@ -1,15 +1,17 @@
 const blogContainer = document.querySelector('.popular-blog-content')
 
+
+
 function popularBlogMethods(){
     fetch('./data/blog.json')
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+    console.log(data)
     data.blogs.forEach(blog => {
     const postEl = document.createElement('div');
     postEl.classList.add('search-card');
     postEl.innerHTML = `
-    
+
     <h3 class="blog-title">${blog.title}</h3>
     <span class="blog-time">${blog.date}</span>
     <span class="blog-view">(${blog.views})</span>
@@ -29,19 +31,36 @@ function popularBlogMethods(){
 
 popularBlogMethods()
 
+const filter = document.getElementById("blog-search")
+const select = document.getElementById("blog-search")
+const listBlog = []
 
-const allBlogContainer = document.querySelector('.all-blog-content');
+filter.addEventListener("input",(e) => {
+    const search = e.target.value.toUpperCase()
+    console.log(search)
+    listBlog.forEach((blog) => {
+        if(blog.innerText.toUpperCase().includes(search)){
+            //show
+          blog.classList.remove("hide")  
+        }else{
+            // hide
+            blog.classList.add("hide")  
+        }
+    })
+})
 
-function allBlogMethods(){
+const allBlogContainer = document.getElementById('all-blog-content');
 
+async function fetchBlog(){
     fetch('./data/blog.json')
     .then(res => res.json())
     .then(data => {
-    blogs = data.blogs.map(blog => {
+    console.log(data)
+    data.blogs.forEach(blog => {
     const postEl = document.createElement('div');
     postEl.classList.add('search-card');
+    listBlog.push(postEl)
     postEl.innerHTML = `
-    
     <h3 class="blog-title">${blog.title}</h3>
     <span class="blog-time">${blog.date}</span>
     <span class="blog-view">(${blog.views})</span>
@@ -54,13 +73,22 @@ function allBlogMethods(){
     <button class="btn">Read More</button>
     </div>`  
     allBlogContainer.appendChild(postEl) 
-    return {title: blog.title,content: blog.content, category: blog.category, author: blog.author}
         })
-    })
+    });
     
-}
 
-allBlogMethods()
+}
+fetchBlog()
+
+
+let blogs = [];
+const maxDisplayLimit = 5;
+
+
+
+// filter for search
+
+
 
 
 
