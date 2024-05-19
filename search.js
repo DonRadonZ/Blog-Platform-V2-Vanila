ec8e6984ca07838159f779dc7a72641297c0bb0c
RadonZ <supachai27102000@gmail.com>
2024-05-19 20:58:17 +0700

    create search sample

---
+++ search.js
@@ -0,0 +1,17 @@
+let search = document.getElementById('blog-search').addEventListener('keyup', (e) => {
+    console.log(e.target.value.toLowerCase())
+});
+
+console.log(search)
+
+function getSelectValue() {
+    var selectedValue = document.getElementById("title").value;
+    console.log(selectedValue);
+}
+
+function searchblog(){
+    
+
+    fetch('./data/blog.json')
+    .then(res => res.json())
+}
